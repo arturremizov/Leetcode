5424396393ce0702705bdb5aab20659bb01a8657
Artur Remizov <arturremizov@gmail.com>
2024-06-02 17:36:35 +0300

    344. Reverse String

---
+++ 344.Reverse-String.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int left = 0, right = s.size() - 1;
+        while (left < right) {
+            swap(s[left], s[right]); 
+            ++left, --right;           
+        }
+    }
+};
+
+int main() 
+{
+    Solution solution; 
+    vector<char> s = {'h','e','l','l','o'};
+    solution.reverseString(s); // "o","l","l","e","h"
+    for (char c : s) {
+        cout << c << endl;
+    }
+}
