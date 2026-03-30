ebad4b36bffa43e8cd9d6e65d6cbb3b9a8b297a2
Artur Remizov <arturremizov@gmail.com>
2026-03-30 17:41:20 +0300

    2840. Check if Strings Can be Made Equal With Operations II

---
+++ 2840.Check-if-Strings-Can-be-Made-Equal-With-Operations-II.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool checkStrings(string s1, string s2) {
+        const int n = s1.length();
+        vector<int> even(26), odd(26);
+        for (int i = 0; i < n; ++i) {
+            if (i % 2 == 0) {
+                even[s1[i] - 'a']++;
+                even[s2[i] - 'a']--;
+            } else {
+                odd[s1[i] - 'a']++;
+                odd[s2[i] - 'a']--;
+            }
+        }
+
+        for (int i = 0; i < 26; ++i) {
+            if (even[i] != 0 || odd[i] != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+int main() {
+    Solution solution; 
+    cout << solution.checkStrings("abcdba", "cabdab") << endl; // true
+}
