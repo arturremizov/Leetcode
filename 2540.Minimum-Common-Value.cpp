29da92cb939a90b1d571145b3526d14552417ec0
Artur Remizov <arturremizov@gmail.com>
2026-05-19 16:09:41 +0300

    2540. Minimum Common Value

---
+++ 2540.Minimum-Common-Value.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        if (nums1.size() > nums2.size()) {
+            return getCommon(nums2, nums1);
+        } 
+        for (int num : nums1) {
+            if (binarySearch(num, nums2)) {
+                return num;
+            }
+        }
+        return -1;
+    }
+private:
+    bool binarySearch(int target, vector<int>& nums) {
+        int left = 0;
+        int right = nums.size() - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) return true;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return false;
+    }
+};
+
+int main() {
+    Solution solution; 
+    vector<int> nums1 = {1,2,3};
+    vector<int> nums2 = {2,4};
+    cout << solution.getCommon(nums1, nums2) << endl; // 2;
+}
